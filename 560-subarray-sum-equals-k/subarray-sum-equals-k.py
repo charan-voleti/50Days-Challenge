781d25b3da0875cd787b5aa8db105741a2f950c7
Charan_Voleti <charanvoleti55@gmail.com>
2025-01-23 18:46:02 +0530

    Time: 33 ms (48.99%) | Memory: 20.3 MB (61.79%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int: 
+        dic = defaultdict(int) # sum 0 occurs once
+        dic[0] = 1
+        runningSum = 0
+        count = 0
+        for num in nums:
+            runningSum += num
+            difference = runningSum - k
+            if difference in dic:
+                count += dic[difference]
+            dic[runningSum] += 1
+        return count
+            
