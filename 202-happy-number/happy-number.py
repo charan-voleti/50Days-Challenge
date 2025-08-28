5ad064e1ea80e61a86630ba9d8efc037faaa4ff4
Charan_Voleti <charanvoleti55@gmail.com>
2025-08-28 20:43:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.09%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,20 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # helper function to calculate sum of squares of digits
-        def get_next(num):
-            total = 0
-            while num > 0:
-                r = num % 10
-                total += r * r
-                num //= 10
-            return total
-
-        slow = n
-        fast = get_next(n)
-
-        # loop until fast reaches 1 (happy number) OR fast meets slow (cycle)
-        while fast != 1 and slow != fast:
-            slow = get_next(slow)           # move 1 step
-            fast = get_next(get_next(fast)) # move 2 steps
-
-        return fast == 1
+        if(n==1 or n==7):
+            return True
+        elif(n<10):
+            return False
+        else:
+            sum =0
+            while(n>0):
+                temp = n%10
+                sum += temp*temp
+                n= n//10
+            return self.isHappy(sum) 
